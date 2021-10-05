64b2ad337eedda516fad8b047b627994d9a3d7ff
TonTon69 <hoanghuynh0609@gmail.com>
2021-10-05 15:57:04 +0700

    update subject show

--- src/app/controllers/SubjectController.js
+++ src/app/controllers/SubjectController.js
@@ -6,13 +6,11 @@ class SubjectController {
     async show(req, res, next) {
         const subject = await Subject.findOne({ slug: req.params.slug });
         const units = await Unit.find({ subjectID: subject.id });
-        // console.log(units);
-        for (const unit of units) {
-            // console.log(unit);
-            let lessions = await Lession.find({ unitID: unit.id });
-            console.log(lessions);
-            res.render("subjects/show", { subject, units, lessions });
-        }
+        const unitIdArray = units.map(({ _id }) => _id);
+        const lessions = await Lession.find({
+            unitID: { $in: unitIdArray },
+        });
+        res.render("subjects/show", { subject, units, lessions });
     }
 }
 
