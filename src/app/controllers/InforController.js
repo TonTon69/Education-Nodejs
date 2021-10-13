9142211ab0d86b56cccd9fe20e73a4fbc3f12058
TonTon69 <hoanghuynh0609@gmail.com>
2021-10-13 21:06:49 +0700

    update infor

--- src/app/controllers/InforController.js
+++ src/app/controllers/InforController.js
@@ -1,15 +1,14 @@
 const User = require("../models/User");
 
 class InforController {
-    // [PUT]/infor/:username
+    // [PUT]/infor/:id
     async update(req, res, next) {
         const user = await User.findOne({ _id: req.signedCookies.userId });
-        User.updateOne({ id: user.id }, req.body)
-            .then(() => {
-                req.flash("success", "Cập nhật thông tin thành công!");
-                res.redirect("/infor");
-            })
-            .catch(next);
+        if (user) {
+            await User.updateOne({ _id: user.id }, req.body);
+            req.flash("success", "Cập nhật thông tin thành công!");
+            res.redirect("/infor");
+        }
     }
 }
 
