5bb9284f5d1b56d6870413f3baece75271b03e31
TonTon69 <hoanghuynh0609@gmail.com>
2021-10-19 22:50:17 +0700

    CRUD units

--- src/app/controllers/UnitController.js
+++ src/app/controllers/UnitController.js
@@ -27,6 +27,9 @@ class UnitController {
                 $limit: perPage,
             },
         ]);
+        const unitSubjectIdArr = units.map(({ subjectID }) => subjectID);
+        const subjects = await Subject.find({ _id: { $in: unitSubjectIdArr } });
+
         const unitsCount = await Unit.countDocuments();
         res.render("units/list", {
             units,
@@ -34,91 +37,147 @@ class UnitController {
             pages: Math.ceil(unitsCount / perPage),
             success: req.flash("success"),
             errors: req.flash("error"),
+            subjects,
         });
     }
 
-    // // [GET]/subjects/list/:page
-    // async pagination(req, res) {
-    //     let perPage = 3;
-    //     let page = req.params.page || 1;
-    //     const subjects = await Subject.find({})
-    //         .sort({ gradeID: -1 })
-    //         .skip(perPage * page - perPage)
-    //         .limit(perPage);
-    //     const subjectsCount = await Subject.countDocuments();
-    //     res.render("subjects/list", {
-    //         subjects,
-    //         current: page,
-    //         pages: Math.ceil(subjectsCount / perPage),
-    //         success: req.flash("success"),
-    //         errors: req.flash("error"),
-    //     });
-    // }
+    // [GET]/units/list/:page
+    async pagination(req, res) {
+        let perPage = 3;
+        let page = req.params.page || 1;
+        const units = await Unit.aggregate([
+            {
+                $lookup: {
+                    from: "subjects",
+                    localField: "subjectID",
+                    foreignField: "_id",
+                    as: "subject",
+                },
+            },
+            {
+                $skip: perPage * page - perPage,
+            },
+            {
+                $limit: perPage,
+            },
+        ]);
+        const unitSubjectIdArr = units.map(({ subjectID }) => subjectID);
+        const subjects = await Subject.find({
+            _id: { $in: unitSubjectIdArr },
+        });
+        const unitsCount = await Unit.countDocuments();
+        res.render("units/list", {
+            units,
+            current: page,
+            pages: Math.ceil(unitsCount / perPage),
+            success: req.flash("success"),
+            errors: req.flash("error"),
+            subjects,
+        });
+    }
 
-    // // [POST]/subjects/list
-    // async searchFilter(req, res) {
-    //     try {
-    //         const searchString = req.body.query;
-    //         const option = req.body.option;
-    //         let subjects = [];
-    //         if (searchString) {
-    //             subjects = await Subject.find({
-    //                 name: { $regex: searchString, $options: "$i" },
-    //             }).sort({ gradeID: -1 });
-    //         } else if (option) {
-    //             subjects = await Subject.find({ gradeID: option });
-    //         } else {
-    //             subjects = await Subject.find({}).sort({ gradeID: -1 });
-    //         }
-    //         res.render("helper/table", { subjects });
-    //     } catch (error) {
-    //         console.log(error);
-    //         req.flash("error", "Không tìm thấy kết quả!");
-    //     }
-    // }
+    // [POST]/units/list
+    async searchFilter(req, res) {
+        try {
+            const searchString = req.body.query;
+            const option = req.body.option;
+            let units = [];
+            if (searchString) {
+                units = await Unit.aggregate([
+                    {
+                        $match: {
+                            name: { $regex: searchString, $options: "$i" },
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: "subjects",
+                            localField: "subjectID",
+                            foreignField: "_id",
+                            as: "subject",
+                        },
+                    },
+                ]);
+            } else if (option) {
+                units = await Unit.aggregate([
+                    {
+                        $match: { subjectID: ObjectId(option) },
+                    },
+                    {
+                        $lookup: {
+                            from: "subjects",
+                            localField: "subjectID",
+                            foreignField: "_id",
+                            as: "subject",
+                        },
+                    },
+                ]);
+            } else {
+                units = await Unit.aggregate([
+                    {
+                        $lookup: {
+                            from: "subjects",
+                            localField: "subjectID",
+                            foreignField: "_id",
+                            as: "subject",
+                        },
+                    },
+                ]);
+            }
+            res.render("helper/table", { units });
+        } catch (error) {
+            console.log(error);
+            req.flash("error", "Không tìm thấy kết quả!");
+        }
+    }
 
-    // // [GET]/subjects/create
-    // async create(req, res) {
-    //     res.render("subjects/create", {
-    //         success: req.flash("success"),
-    //     });
-    // }
+    // [GET]/units/create
+    async create(req, res) {
+        const subjects = await Subject.find({});
 
-    // // [POST]/subjects/create
-    // async postCreate(req, res) {
-    //     const subject = new Subject(req.body);
-    //     await subject.save();
-    //     req.flash("success", "Thêm mới thành công!");
-    //     res.redirect("back");
-    // }
+        res.render("units/create", {
+            subjects,
+            success: req.flash("success"),
+        });
+    }
 
-    // // [GET]/subjects/:id/edit
-    // async edit(req, res, next) {
-    //     const subject = await Subject.findById(req.params.id);
-    //     res.render("subjects/edit", {
-    //         subject,
-    //         success: req.flash("success"),
-    //     });
-    // }
+    // [POST]/units/create
+    async postCreate(req, res) {
+        const unit = new Unit(req.body);
+        await unit.save();
+        req.flash("success", "Thêm mới thành công!");
+        res.redirect("back");
+    }
 
-    // // [PUT]/subjects/:id
-    // async update(req, res, next) {
-    //     await Subject.updateOne({ _id: req.params.id }, req.body);
-    //     req.flash("success", "Cập nhật thành công!");
-    //     res.redirect("back");
-    // }
+    // [GET]/units/:id/edit
+    async edit(req, res, next) {
+        const subjects = await Subject.find({});
+        const unit = await Unit.findById(req.params.id);
+        res.render("units/edit", {
+            subjects,
+            unit,
+            success: req.flash("success"),
+        });
+    }
 
-    // // [DELETE]/subjects/:id
-    // async delete(req, res, next) {
-    //     try {
-    //         await Subject.deleteOne({ _id: req.params.id });
-    //         req.flash("success", "Xóa thành công!");
-    //         res.redirect("back");
-    //     } catch (error) {
-    //         req.flash("error", "Xóa thất bại!");
-    //         res.redirect("back");
-    //     }
-    // }
+    // [PUT]/units/:id
+    async update(req, res, next) {
+        await Unit.updateOne({ _id: req.params.id }, req.body);
+        req.flash("success", "Cập nhật thành công!");
+        res.redirect("back");
+    }
+
+    // [DELETE]/units/:id
+    async delete(req, res, next) {
+        try {
+            await Unit.deleteOne({ _id: req.params.id });
+            req.flash("success", "Xóa thành công!");
+            res.redirect("back");
+        } catch (error) {
+            req.flash("error", "Xóa thất bại!");
+            res.redirect("back");
+        }
+    }
 }
 
 module.exports = new UnitController();
--- src/routes/units.js
+++ src/routes/units.js
@@ -3,14 +3,13 @@ const router = express.Router();
 
 const unitController = require("../app/controllers/UnitController");
 
-// router.get("/create", unitController.create);
-// router.post("/create", unitController.postCreate);
+router.get("/create", unitController.create);
+router.post("/create", unitController.postCreate);
 router.get("/list", unitController.list);
-// router.get("/list/:page", unitController.pagination);
-// router.post("/list", unitController.searchFilter);
-// router.get("/:id/edit", unitController.edit);
-// router.put("/:id", unitController.update);
-// router.delete("/:id", unitController.delete);
-// router.get("/:slug", unitController.show);
+router.get("/list/:page", unitController.pagination);
+router.post("/list", unitController.searchFilter);
+router.get("/:id/edit", unitController.edit);
+router.put("/:id", unitController.update);
+router.delete("/:id", unitController.delete);
 
 module.exports = router;
