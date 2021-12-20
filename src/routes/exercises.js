7dd82ea2fb1ec9d9aa90f803ebae22dbfd6aa066
TonTon69 <hoanghuynh0609@gmail.com>
2021-12-20 15:45:58 +0700

    update download file

--- src/routes/exercises.js
+++ src/routes/exercises.js
@@ -3,20 +3,24 @@ const router = express.Router();
 
 const exerciseController = require("../app/controllers/ExerciseController");
 
-const { requireAuth } = require("../app/middlewares/AuthMiddleware");
+const {
+    requireAuth,
+    checkAdmin,
+} = require("../app/middlewares/AuthMiddleware");
 const upload = require("../app/middlewares/upload");
 
-router.post("/:id/export", requireAuth, exerciseController.export);
+router.post("/:id/export", requireAuth, checkAdmin, exerciseController.export);
 router.post(
     "/upload",
     requireAuth,
+    checkAdmin,
     upload.single("file"),
     exerciseController.upload
 );
-router.get("/create", requireAuth, exerciseController.create);
-router.post("/create", requireAuth, exerciseController.postCreate);
-router.get("/detail", requireAuth, exerciseController.detail);
-router.delete("/:id", requireAuth, exerciseController.delete);
-router.put("/:id", requireAuth, exerciseController.update);
+router.get("/create", requireAuth, checkAdmin, exerciseController.create);
+router.post("/create", requireAuth, checkAdmin, exerciseController.postCreate);
+router.get("/detail", requireAuth, checkAdmin, exerciseController.detail);
+router.delete("/:id", requireAuth, checkAdmin, exerciseController.delete);
+router.put("/:id", requireAuth, checkAdmin, exerciseController.update);
 
 module.exports = router;
--- src/routes/theories.js
+++ src/routes/theories.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
+const {
+    checkAdmin,
+    requireAuth,
+} = require("../app/middlewares/AuthMiddleware");
 const theoryController = require("../app/controllers/TheoryController");
 
-router.post("/:id/export", theoryController.generatePdf);
-router.get("/create", theoryController.create);
-router.post("/create", theoryController.postCreate);
-router.get("/detail", theoryController.detail);
-router.delete("/:id", theoryController.delete);
-router.put("/:id", theoryController.update);
+router.post("/:id/export", requireAuth, theoryController.generatePdf);
+router.get("/create", requireAuth, checkAdmin, theoryController.create);
+router.post("/create", requireAuth, checkAdmin, theoryController.postCreate);
+router.get("/detail", requireAuth, checkAdmin, theoryController.detail);
+router.delete("/:id", requireAuth, checkAdmin, theoryController.delete);
+router.put("/:id", requireAuth, checkAdmin, theoryController.update);
 
 module.exports = router;
